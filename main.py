482c61294c8f6e71ad88ddcc0388d2e2b5d5ac11
AkaBA3u <arseniyshalashov0402@yandex.ru>
2022-12-16 18:50:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+
+    fps = 60
+    clock = pygame.time.Clock()
+
+    running = True
+
+    while running:
+        screen.fill('black')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+            if event.type == pygame.MOUSEBUTTONUP:
+                pass
+            if event.type == pygame.MOUSEMOTION:
+                pass
+
+        pygame.display.flip()
+
+        clock.tick(fps)
+    pygame.quit()
